9ea985249a6d23ab512207eda494f5078a60e9a5
so_young <qksthdhd94@gmail.com>
2019-11-14 19:56:46 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a20191114_01_okhttp/BaseActivity.kt
@@ -0,0 +1,15 @@
+package com.example.a20191114_01_okhttp
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity :AppCompatActivity() {
+
+    var mContext:Context = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+
+
+}
