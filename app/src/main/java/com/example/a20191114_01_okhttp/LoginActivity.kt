0c3f0d382af029d6b12e4a62a513a2c74b3e8cee
so_young <qksthdhd94@gmail.com>
2019-11-14 21:39:00 +0900

    로그인 응답에 따른 다른 Toast 띄우기

--- app/src/main/java/com/example/a20191114_01_okhttp/LoginActivity.kt
+++ app/src/main/java/com/example/a20191114_01_okhttp/LoginActivity.kt
@@ -1,8 +1,11 @@
 package com.example.a20191114_01_okhttp
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.a20191114_01_okhttp.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_login.*
+import org.json.JSONObject
 
 class LoginActivity : BaseActivity() {
 
@@ -20,7 +23,25 @@ class LoginActivity : BaseActivity() {
 
             var inputId = userIdEdt.text.toString()
             var inputPw = userPwEdt.text.toString()
-            ServerUtil.postRequestLogin(mContext,inputId,inputPw,null)
+            ServerUtil.postRequestLogin(mContext,inputId,inputPw,object : ServerUtil.JsonResponseHandler{
+                override fun onResponse(json: JSONObject) {
+
+                    runOnUiThread {
+                        Log.d("액티비티에서 보는 응답",json.toString())
+
+                        var code = json.getInt("code")
+
+                        if(code==400)
+                        {
+                            var message = json.getString("message")
+                            Toast.makeText(mContext,message,Toast.LENGTH_SHORT).show()
+                        }else if(code == 200){
+                            Toast.makeText(mContext,"로그인 성공",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                    }
+
+            })
 
         }
 
